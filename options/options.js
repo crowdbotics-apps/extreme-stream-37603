a53f8c88b64d07ef5a176a2a7fdf5c3d82449d62
Crowdbotics <team@crowdbotics.com>
2022-11-16 00:07:58 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "extreme_stream_37603",
+  url: "https://extreme_stream_37603.botics.co",
+  api: "https://extreme_stream_37603.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
